a50d5dc4391ae485d049bdf2f8f66b6dc1fb53bd
DrJupiter <klausjupiter@hotmail.com>
2020-11-09 17:41:56 +0100

    Test eval file

---
+++ src/testmodel.py
@@ -0,0 +1,39 @@
+import torch
+from imageio import imread
+
+import sys
+
+PATH_TO_MODEL = sys.argv[1]
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+IMG_FLATTEN = 256*256
+H = 10
+
+model = torch.nn.Sequential(
+torch.nn.Linear(IMG_FLATTEN, H),
+torch.nn.Tanh(),
+torch.nn.Linear(H, H),
+torch.nn.Tanh(),
+torch.nn.Linear(H, H),
+torch.nn.Tanh(),
+torch.nn.Linear(H, H),
+torch.nn.Tanh(),
+torch.nn.Linear(H, H),
+torch.nn.Tanh(),
+torch.nn.Linear(H, H),
+torch.nn.Tanh(),
+torch.nn.Linear(H, 2)
+)
+
+model.to(device)
+
+model.load_state_dict(torch.load(PATH_TO_MODEL))
+
+model.eval()
+
+img = imread(sys.argv[2])
+data = torch.from_numpy(img.flatten())
+data = data.type(torch.FloatTensor).to(device)
+
+print(model(data))
